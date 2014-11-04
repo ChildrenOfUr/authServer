59b3678349cd42685c91e121e582aabfb95658e3
Robert McDermot <robert.mcdermot@gmail.com>
2014-11-04 11:56:23 -0500

    added CORS headers to the authServer requests

--- bin/authserver.dart
+++ bin/authserver.dart
@@ -7,6 +7,7 @@ import 'dart:math';
 
 import "package:http/http.dart" as http;
 import "package:redstone/server.dart" as app;
+import 'package:shelf/shelf.dart' as shelf;
 
 part "../API_KEYS.dart";
 part "auth.dart";
@@ -21,3 +22,22 @@ void main()
 	app.setupConsoleLog();
 	app.start(port:8383, autoCompress:true, secureOptions: {#certificateName: "robertmcdermot.com"});
 }
+
+//add a CORS header to every request
+@app.Interceptor(r'/.*')
+crossOriginInterceptor()
+{
+	if (app.request.method == "OPTIONS")
+	{
+		//overwrite the current response and interrupt the chain.
+		app.response = new shelf.Response.ok(null, headers: _createCorsHeader());
+		app.chain.interrupt();
+	}
+	else
+	{
+  	//process the chain and wrap the response
+		app.chain.next(() => app.response.change(headers: _createCorsHeader()));
+	}
+}
+
+_createCorsHeader() => {"Access-Control-Allow-Origin": "*","Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"};
