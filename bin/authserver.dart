9b5f8fc42f9bb84751b68d77df4b090271fadb6d
Robert McDermot <robert.mcdermot@gmail.com>
2015-12-27 11:40:07 -0500

    switch to new ssl style and new cert

--- bin/authserver.dart
+++ bin/authserver.dart
@@ -20,9 +20,13 @@ export '../API_KEYS.dart';
 //for the test suite
 
 part '../lib/auth.dart';
+
 part '../lib/data.dart';
+
 part '../lib/user.dart';
+
 part '../lib/metabolics.dart';
+
 part '../lib/verify_handler.dart';
 
 Map<String, Session> SESSIONS = {};
@@ -35,89 +39,91 @@ File verifiedOutputFile, errorOutputFile;
 String verifiedOutput, errorOutput;
 
 Future main(List<String> arguments) async {
-	//setup command line argument parsing
-	final parser = new ArgParser()
-	//use --no-load-cert to ignore certification loading
-		..addFlag("load-cert", defaultsTo: true, help: "Enables certificate loading for certificate")
-		..addOption("port", defaultsTo:"8383", help: "Port to run the server on");
+    //setup command line argument parsing
+    final parser = new ArgParser()
+    //use --no-load-cert to ignore certification loading
+        ..addFlag("load-cert", defaultsTo: true, help: "Enables certificate loading for certificate")
+        ..addOption("port", defaultsTo: "8383", help: "Port to run the server on");
 
-	argResults = parser.parse(arguments);
-	loadCert = argResults['load-cert'];
+    argResults = parser.parse(arguments);
+    loadCert = argResults['load-cert'];
 
-	int port;
-	try {
-		port = int.parse(argResults['port']);
-	}
-	catch(error) {
-		port = 8383;
-	}
+    int port;
+    try {
+        port = int.parse(argResults['port']);
+    }
+    catch (error) {
+        port = 8383;
+    }
 
-	//try to parse ENV var
-	if(Platform.environment['AUTH_PORT'] != null &&
-	   Platform.environment['AUTH_PORT'].isNotEmpty) {
-		try {
-			port = int.parse(Platform.environment['AUTH_PORT']);
-		}
-		catch(error) {
-			port = 8383;
-		}
-	}
+    //try to parse ENV var
+    if (Platform.environment['AUTH_PORT'] != null &&
+        Platform.environment['AUTH_PORT'].isNotEmpty) {
+        try {
+            port = int.parse(Platform.environment['AUTH_PORT']);
+        }
+        catch (error) {
+            port = 8383;
+        }
+    }
 
-	dbManager = new PostgreSqlManager(databaseUri, min: 1, max: 9);
-	app.addPlugin(getMapperPlugin(dbManager));
+    dbManager = new PostgreSqlManager(databaseUri, min: 1, max: 9);
+    app.addPlugin(getMapperPlugin(dbManager));
 
-	if(loadCert) {
-		try {
-			String certPath = 'ssl/letsencrypt/server.childrenofur.com';
-			SecurityContext context = new SecurityContext()
-				..useCertificateChain('$certPath/fullchain.pem')
-				..usePrivateKey('$certPath/privkey.pem');
-			app.setupConsoleLog();
-			app.start(port:port, autoCompress:true, secureOptions: {#context: context});
-		} catch(error) {
-			print("Unable to start server with signed certificate: $error");
-		}
-	}
-	else {
-		//start up server in non-cert-certified developer mode
-		app.setupConsoleLog();
-		app.start(port:port);
-	}
+    if (loadCert) {
+        try {
+            SecurityContext context = new SecurityContext()
+                ..useCertificateChain('$certPath/fullchain.pem')
+                ..usePrivateKey('$certPath/privkey.pem');
+            app.setupConsoleLog();
+            app.start(port: port, autoCompress: true, secureOptions: {#context: context});
+        } catch (error) {
+            print("Unable to start server with signed certificate: $error");
+        }
+    }
+    else {
+        //start up server in non-cert-certified developer mode
+        app.setupConsoleLog();
+        app.start(port: port);
+    }
 
-	VerifyHandler.init();
+    VerifyHandler.init();
 
-	String currentDir = path.dirname(Platform.script.path);
-	verifiedOutputFile = new File("$currentDir/output_verified.html");
-	errorOutputFile = new File("$currentDir/output_error.html");
+    String currentDir = path.dirname(Platform.script.path);
+    verifiedOutputFile = new File("$currentDir/output_verified.html");
+    errorOutputFile = new File("$currentDir/output_error.html");
 
-	verifiedOutput = await (verifiedOutputFile.readAsString());
-	errorOutput = await (errorOutputFile.readAsString());
+    verifiedOutput = await (verifiedOutputFile.readAsString());
+    errorOutput = await (errorOutputFile.readAsString());
 }
 
 //add a CORS header to every request
 @app.Interceptor(r'/.*')
 handleCORS() async {
-	if (app.request.method != "OPTIONS") {
-		await app.chain.next();
-	}
-	return app.response.change(headers: {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"});
+    if (app.request.method != "OPTIONS") {
+        await app.chain.next();
+    }
+    return app.response.change(headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+    });
 }
 
 PostgreSql get dbConn => app.request.attributes.dbConn;
 
 @app.Route('/serverStatus')
 Map getServerStatus() {
-	Map statusMap = {};
-	try {
-		statusMap['status'] = "OK";
-		statusMap['loadCert'] = loadCert;
-	}
-	catch(e) {
-		logMessage("Error getting server status: $e");
-	}
-	return statusMap;
+    Map statusMap = {};
+    try {
+        statusMap['status'] = "OK";
+        statusMap['loadCert'] = loadCert;
+    }
+    catch (e) {
+        logMessage("Error getting server status: $e");
+    }
+    return statusMap;
 }
 
 void logMessage(String message) {
-	print("(${new DateTime.now().toString()}) $message");
+    print("(${new DateTime.now().toString()}) $message");
 }
