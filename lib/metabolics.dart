84a0af47f3ad55266fc1788a9934a2cab71d0bce
Andy Castille <klikini@robiotic.net>
2017-04-09 15:25:30 -0500

    rename metabolics fields

--- lib/metabolics.dart
+++ lib/metabolics.dart
@@ -1,8 +1,11 @@
 part of authServer;
 
 class Metabolics {
-    @Field() int mood = 50, max_mood = 100, energy = 50, max_energy = 100, currants = 0;
-    @Field() int img = 0, lifetime_img = 0, user_id = -1;
-    @Field() String current_street = 'LIF12PMQ5121D68';
-    @Field() num current_street_x = 1.0, current_street_y = 0.0;
+    @Field() int userId = -1;
+    @Field() int energy = 50, maxEnergy = 100;
+    @Field() int mood = 50, maxMood = 100;
+    @Field() int currants = 0;
+    @Field() int img = 0, lifetimeImg = 0;
+    @Field() String currentStreet = 'LIF12PMQ5121D68';
+    @Field() num currentStreetX = 1.0, currentStreetY = 0.0;
 }
