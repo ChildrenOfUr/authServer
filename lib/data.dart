ebc378eb1f7220377bd0cf6304fca3f72ecf789d
Andy Castille <klikini@robiotic.net>
2017-04-19 22:36:20 -0500

    allow client to specify streetdata branch

--- lib/data.dart
+++ lib/data.dart
@@ -3,23 +3,25 @@ part of authServer;
 @app.Group('/data')
 class DataService {
     @app.Route('/street', methods: const[app.POST])
-    Future<Map> requestStreet(@app.Body(app.JSON) Map parameters) async
-    {
-        if (!SESSIONS.containsKey(parameters['sessionToken']))
-            return {'ok':'no', 'error':'not logged in'};
-        else {
+    Future<Map> requestStreet(@app.Body(app.JSON) Map parameters) async {
+        if (!SESSIONS.containsKey(parameters['sessionToken'])) {
+	        return {'ok': 'no', 'error': 'not logged in'};
+        } else {
             String tsid = parameters['street'];
-            if (tsid.startsWith('L'))
+            String branch = parameters['branch'] ?? 'master';
+
+            if (tsid.startsWith('L')) {
                 tsid = tsid.replaceFirst('L', 'G');
-            String url = "https://raw.githubusercontent.com/ChildrenOfUr/CAT422-glitch-location-viewer/master/locations/$tsid.json";
+            }
+
+            String url = "https://raw.githubusercontent.com/ChildrenOfUr/CAT422-glitch-location-viewer/$branch/locations/$tsid.json";
             http.Response response = await http.get(url);
 
             try {
                 Map street = JSON.decode(response.body);
-                return {'ok':'yes', 'streetJSON':street};
-            }
-            catch (err) {
-                return {'ok':'no', 'error':err};
+                return {'ok': 'yes', 'streetJSON': street};
+            } catch (err) {
+                return {'ok': 'no', 'error': err};
             }
         }
     }
